f44fb719881af3ec6de47c89b2213e315ed29ced
Ike Maldonado <maldonadoike@gmail.com>
2024-11-13 11:01:51 -0700

    fib

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+# O(c^N) - Exponential
+
+
+def fibonacci(n):
+    if (1 == n) | (2 == n):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if "__name__" == "__main__":
+    print(fibonacci(3))
